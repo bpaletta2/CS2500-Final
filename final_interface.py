1e8114aaaa502f2bb9a6658b579b3e9e823c476f
alex-shapovalov <shapovalov.alex@pm.me>
2024-11-20 16:36:35 -0500

    Baseline code

--- final_interface.py
+++ final_interface.py
@@ -6,36 +6,71 @@ import numpy as np
 import datetime
 import pwinput
 
+from csv_to_sql import csv_to_sql
+
 def login():
-    check = False
-    checkTwo = False
-    username = ""
-    while check == False:
-        print("Enter your username: ")
-        username = input()
-        s = "SELECT * FROM users WHERE username =  '" + username + "'"
-        cur.execute(s)
+    username_check = False
+    while username_check == False:
+        username = input("Enter your username: ")
+        cur.execute(f"SELECT * FROM user_account WHERE username = '{username}'")
         f = cur.fetchall()
         if len(f) == 0:
-            print("User not found")
+            print("User doesn't exist")
         if len(f) != 0:
-            check = True
+            username_check = True
 
-    while checkTwo == False:
-        password = pwinput.pwinput(prompt="Enter your password: ")
-        s = "SELECT password FROM users WHERE username =  '" + username + "'"
-        cur.execute(s)
-        f = cur.fetchall()
-        pass2 = f[0]
-        result = ''.join(pass2)
-
-        if password == result:
-            print("Successful Login\n")
+    while True:
+        password = pwinput.pwinput("Enter password: ")
+        cur.execute(f"SELECT password FROM user_account WHERE username =  '{username}'")
+        f = cur.fetchone()
+        if password == f[0]:
+            print("Successful login\n")
             return username
         else:
-            print("Access Denied")
+            print("Incorrect password")
 
 if __name__ == "__main__":
+    csv_to_sql()
     conn = sqlite3.connect('final_project.db', isolation_level = None)
     cur = conn.cursor()
-    login()
+    username = login()
+
+    while True:
+        print("a) Read all user information")
+        print("b) Modify Account")
+        print("c) Deposit / withdraw money")
+        print("d) Make a credit payment")
+        print("e) Exit")
+        # print("z) Admin")
+        print("")
+
+        user_input = input("Enter your choice: ")
+
+        if user_input == "a":
+            print("")
+
+        elif user_input == "b":
+            print("")
+
+        elif user_input == "c":
+            print("")
+
+        elif user_input == "d":
+            print("")
+
+        elif user_input == "e":
+            print("")
+            print("Exiting app...")
+            print("")
+            break
+
+        # elif user_input == "z":
+        #     print("")
+
+        else:
+            print("Invalid input, try again")
+            print("")
+
+    cur.commit()
+    cur.close()
+
