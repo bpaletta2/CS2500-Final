0df049f03498401928c678c4cb4f951271a0f8ae
bpaletta2 <156727071+bpaletta2@users.noreply.github.com>
2024-11-20 15:51:44 -0500

    Add files via upload

---
+++ csv_to_sql.py
@@ -0,0 +1,36 @@
+import sqlite3
+import csv
+import time
+import pandas as pd 
+import numpy as np
+
+conn = sqlite3.connect('final_project.db', isolation_level = None)
+cur = conn.cursor()
+
+
+file = open('user_account.csv')
+contents = csv.reader(file)
+userData = pd.read_csv('user_account.csv')
+userData.to_sql('user_account', conn, if_exists='replace', index = True, index_label = 'number')
+file.close()
+
+
+file = open('bank_account.csv')
+contents = csv.reader(file)
+followers = pd.read_csv('bank_account.csv')
+followers.to_sql('bank_account', conn, if_exists='replace', index = True, index_label = 'number')
+file.close()
+
+
+file = open('credit_card.csv')
+contents = csv.reader(file)
+posts = pd.read_csv('credit_card.csv')
+posts.to_sql('credit_card', conn, if_exists='replace', index = True, index_label = 'number')
+file.close()
+
+
+file = open('purchase.csv')
+contents = csv.reader(file)
+posts = pd.read_csv('purchase.csv')
+posts.to_sql('purchase', conn, if_exists='replace', index = True, index_label = 'number')
+file.close()
---
+++ final_interface.py
@@ -0,0 +1,39 @@
+import sqlite3
+import csv
+import time
+import pandas as pd 
+import numpy as np
+import datetime
+import pwinput
+
+def login():
+    check = False
+    checkTwo = False
+    username = ""
+    while check == False:
+        print("Enter your username: ")
+        username = input()
+        s = "SELECT * FROM users WHERE username =  '" + username + "'"
+        cur.execute(s)
+        f = cur.fetchall()
+        if len(f) == 0:
+            print("User not found")
+        if len(f) != 0:
+            check = True
+
+    while checkTwo == False:
+        password = pwinput.pwinput(prompt="Enter your password: ")
+        s = "SELECT password FROM users WHERE username =  '" + username + "'"
+        cur.execute(s)
+        f = cur.fetchall()
+        pass2 = f[0]
+        result = ''.join(pass2)
+
+        if password == result:
+            print("Successful Login\n")
+            return username
+        else:
+            print("Access Denied")
+
+if __name__ == "__main__":
+    login()
